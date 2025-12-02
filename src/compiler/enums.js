5fdf9f262107e81a7ae583e762cc1e78bfb0924d
Cubester <cubester.spamlegends@gmail.com>
2025-12-02 10:32:18 -0500

    Jwklong made me do it

--- src/compiler/enums.js
+++ src/compiler/enums.js
@@ -84,17 +84,17 @@ const InputType = {
     /** Any input that can be interperated as a boolean. Equal to BOOLEAN | STRING_BOOLEAN */
     BOOLEAN_INTERPRETABLE: 0x1800,
 
-    /** Any value type (a type a scratch variable can hold). Equal to NUMBER_OR_NAN | STRING | BOOLEAN */
-    ANY: 0x1FFF,
-
-    /** An array of values in the form [R, G, B] */
-    COLOR: 0x2000,
-
     /** Any object. */
-    OBJECT: 0x4000,
+    OBJECT: 0x2000,
 
     /** Any array. */
-    ARRAY: 0x8000
+    ARRAY: 0x4000,
+
+    /** Any value type (a type a scratch variable can hold). Equal to NUMBER_OR_NAN | STRING | BOOLEAN | OBJECT | ARRAY */
+    ANY: 0x7FFF,
+
+    /** An array of values in the form [R, G, B] */
+    COLOR: 0x8000
 };
 
 /**
